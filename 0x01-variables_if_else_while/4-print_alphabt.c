9bc767e6eac531bac3b66e45ed2bf15ff2493b63
angela <angelngina98@gmail.com>
2023-02-17 15:45:42 -0800

    prints alphabets in lowercase missing 2 letters

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,10 +9,10 @@ int main(void)
 	char alp[24] = "abcdfghijklmnoprstuvwxyz";
 	int i;
 
-	for( i = 0; i < 24; i++)
+	for (i = 0; i < 24; i++)
 	{
 		putchar(alp[i]);
 	}
 	putchar('\n');
-	return( 0);
+	return (0);
 }
